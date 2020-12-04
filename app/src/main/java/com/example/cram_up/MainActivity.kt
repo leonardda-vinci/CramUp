b3e4e9c2a96f7644a91e608f115ee08e994d8e1e
leonardda-vinci <68801076+leonardda-vinci@users.noreply.github.com>
2020-12-04 17:26:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cram_up/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.cram_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment= Home()
+        val secondFragment= assignments()
+        val thirdFragment= Settings()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(firstFragment)
+                R.id.assignments->setCurrentFragment(secondFragment)
+                R.id.settings->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
